f008227e649baebcce5ba1c176269c36f02ba999
sweetDeliven <1214621260@qq.com>
2018-09-02 13:57:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hp/tpp/BuySucess.java
@@ -0,0 +1,22 @@
+package com.example.hp.tpp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class BuySucess extends AppCompatActivity {
+ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_sucess);
+        back =findViewById(R.id.sucsess_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
